9fedb1840e9f11ef94c4031b8d2273ce653e28cb
Marcos Macedo <marcosmacedo.fogao@gmail.com>
2025-03-20 16:07:50 -0300

    Correção na query do produto

--- template-parts/page/obrigado.php
+++ template-parts/page/obrigado.php
@@ -1,6 +1,6 @@
 <?php
 
-add_action('template_redirect', function() {
+add_action('template_redirect', function () {
     if (is_page('obrigado')) {
         // if (!isset($_GET['receipt_url']) || !isset($_GET['transaction_id'])) {
         //     wp_redirect(home_url());
@@ -20,36 +20,26 @@ add_action('template_redirect', function() {
         $quantidade = 1;
 
         // Buscar o produto pela descrição
-        $args = [
-            'post_type'      => 'product',
-            'posts_per_page' => 1,
-            'p'              => 69
-        ];
-        $query = new WP_Query($args);
+        $product = wc_get_product(69);
+        log_to_file('-----------------------------');
+        log_to_file(print_r($product , true));
+        $order = wc_create_order([
+            'customer_id' => $user_id
+        ]);
+        log_to_file(print_r($order, true));
+        $order->add_product(wc_get_product(69), $quantidade);
+        $order->set_payment_method('infinitepay');
+        $order->set_payment_method_title('InfinitePay');
+        $order->calculate_totals();
+        $order->update_status('processing', 'Pagamento confirmado via InfinitePay.');
 
-        if ($query->have_posts()) {
-            $query->the_post();
-            $product_id = get_the_ID();
-
-            // Criar o pedido
-            $order = wc_create_order([
-                'customer_id' => $user_id
-            ]);
-            log_to_file(print_r($order ,true));
-            $order->add_product(wc_get_product($product_id), $quantidade);
-            $order->set_payment_method('infinitepay');
-            $order->set_payment_method_title('InfinitePay');
-            $order->calculate_totals();
-            $order->update_status('processing', 'Pagamento confirmado via InfinitePay.');
-
-            // Redirecionar para a página de obrigado com o ID do pedido
-            $order_id = $order->get_id();
-            // wp_redirect(add_query_arg('order_id', $order_id, get_permalink()));
-            exit;
-        } else {
-            wp_redirect(home_url());
-            exit;
-        }
+        // Redirecionar para a página de obrigado com o ID do pedido
+        $order_id = $order->get_id();
+        // wp_redirect(add_query_arg('order_id', $order_id, get_permalink()));
+        exit;
+    } else {
+        wp_redirect(home_url());
+        exit;
     }
     if (isset($_GET['order_id'])) {
         $order = wc_get_order(intval($_GET['order_id']));
